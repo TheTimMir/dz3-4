464e64046dd4d821665f5a83f4341399cda4fd2d
Тим <mirocnicencotimofej2008@gmail.com>
2022-09-18 09:49:09 +0300

    Добавлен демонстративный код для класса Samurai

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from character import Character, Berserk
+from character import *
 
 player_1 = Character(name='Vasya', damage=2)
 print(player_1.stats())
-player_2 = Berserk(name='Petya', hp=20, damage=3)
+player_2 = Samurai(name='Petya', hp=20, damage=5)
 print(player_2.stats())
 
-for i in range(3):
+for i in range(7):
     print(f' - РАУНД {i+1}')
     player_1.attack(player_2)
     player_2.attack(player_1)
